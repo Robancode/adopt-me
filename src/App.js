e376965826907b39f27ea019799948c63a6474d5
rosita bannert <rosita.bannert@concentra.co.uk>
2019-09-30 15:25:17 +0100

    add hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1 id="sometihng-important">Adopt Me!</h1>
-      <Pet name="catty" animal="cat" breed="cute" />
-      <Pet name="nelly" animal="cat" breed="tortoiseshell" />
-      <Pet name="judy" animal="cat" breed="mix" />
+      <SearchParams />
     </div>
   );
 };
