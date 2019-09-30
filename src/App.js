d6aa1f8d3e89e4587412edb4ff8a2b223e948b0a
rosita bannert <rosita.bannert@concentra.co.uk>
2019-09-30 15:12:25 +0100

    jsx updates

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, { name: "Cattie", animal: "cat", breed: "rogue" }),
-    React.createElement(Pet, { name: "Judy", animal: "cat", breed: "beauty" }),
-    React.createElement(Pet, {
-      name: "nelly",
-      animal: "cat",
-      breed: "tortoiseshell"
-    })
-  ]);
+  return (
+    <div>
+      <h1 id="sometihng-important">Adopt Me!</h1>
+      <Pet name="catty" animal="cat" breed="cute" />
+      <Pet name="nelly" animal="cat" breed="tortoiseshell" />
+      <Pet name="judy" animal="cat" breed="mix" />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
